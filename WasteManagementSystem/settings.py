922028c99fe4053409756135a586afd482268011
ErnestCrypto <akotobamfo.eab@gmail.com>
2022-04-05 02:29:42 +0000

    commit

--- WasteManagementSystem/settings.py
+++ WasteManagementSystem/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Admin.apps.AdminConfig',
+    'Users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
