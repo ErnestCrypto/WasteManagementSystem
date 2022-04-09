93809846b75a10e456fcf75fc591fdb615749a84
ErnestCrypto <akotobamfo.eab@gmail.com>
2022-04-09 05:44:26 +0000

    commit

--- Admin/api_views.py
+++ Admin/api_views.py
@@ -1,6 +1,6 @@
 # Creating our API views
 from django.http import Http404
-from rest_framework import status
+from rest_framework import status, generics
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -14,51 +14,19 @@ List all models instances or create a new instance
 """
 
 
-class Adminstrators_details(APIView):
-    # If request is a Get method
-    def get(self, request, format=None):
-        administrators = Administrators.objects.all()
-        serializer = AdministratorsSerializer(administrators, many=True)
-        return Response(serializer.data)
-
-    # If request is a POST method
-    def post(self, request, format=None):
-        serializer = AdministratorsSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class Adminstrators_details(generics.ListCreateAPIView):
+    queryset = Administrators.objects.all()
+    serializer_class = AdministratorsSerializer
 
 
-class Drivers_details(APIView):
-    # if request is a get method
-    def get(self, request, format=None):
-        drivers = Drivers.objects.all()
-        serializer = DriversSerializer(drivers, many=True)
-        return Response(serializer.data)
-
-    # if request is a post method
-    def post(self, request, format=None):
-        serializer = DriversSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class Drivers_details(generics.ListCreateAPIView):
+    queryset = Drivers.objects.all()
+    serializer_class = DriversSerializer
 
 
-class Trucks_details(APIView):
-    # if request is a get method
-    def get(self, request, format=None):
-        trucks = Trucks.objects.all()
-        serializers = TrucksSerializer(trucks, many=True)
-        return Response(serializers.data)
-
-    def post(self, request, format=None):
-        serializer = TrucksSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class Trucks_details(generics.ListCreateAPIView):
+    queryset = Trucks.objects.all()
+    serializer_class = TrucksSerializer
 
 
 class Pricings_details(APIView):
