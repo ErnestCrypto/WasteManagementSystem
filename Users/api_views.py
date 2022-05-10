93ee2a1ba5a0d86ba7e5b7340e3a06b7eb3d5f3b
ErnestCrypto <akotobamfo.eab@gmail.com>
2022-05-10 00:58:51 +0000

    commit

--- Users/api_views.py
+++ Users/api_views.py
@@ -17,15 +17,6 @@ import datetime
 """
     List all the models instance or create a new model instance
 """
-"""
-if request.method == 'POST':
-    jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
-    jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
-
-    payload = jwt_payload_handler(request.user)
-    token = jwt_encode_handler(payload)
-    return HttpResponse('Get token auth request and data is as: {}'.format(token))
-"""
 
 
 class Test(APIView):
